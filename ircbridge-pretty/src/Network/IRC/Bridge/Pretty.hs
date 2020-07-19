a0409dcc288443a5100b72d3f032bf5ba9e85ceb
Richard Marko <srk@48.io>
2020-07-19 13:17:14 +0200

    pretty: ocd

--- ircbridge-pretty/src/Network/IRC/Bridge/Pretty.hs
+++ ircbridge-pretty/src/Network/IRC/Bridge/Pretty.hs
@@ -9,6 +9,7 @@ module Network.IRC.Bridge.Pretty (
   ) where
 
 import Data.Text (Text)
+import Data.Time.Clock (UTCTime)
 import Data.Time.Format.ISO8601
 
 import Data.Text.Prettyprint.Doc
@@ -17,12 +18,13 @@ import Data.Text.Prettyprint.Doc.Render.Text
 import Network.IRC.Bridge.Types
 
 renderOutput = renderStrict . layoutPretty defaultLayoutOptions . prettyOutput
-renderInput = renderStrict . layoutPretty defaultLayoutOptions . prettyInput
+renderInput  = renderStrict . layoutPretty defaultLayoutOptions . prettyInput
 
 prettyTarget :: IRCTarget -> Doc ann
 prettyTarget (IRCUser    u) = "user:" <+> pretty u
 prettyTarget (IRCChannel c) = "chan:" <+> pretty c
 
+prettyTime :: UTCTime -> Doc ann
 prettyTime = brackets . pretty . iso8601Show
 
 prettySender (Just u) = braces ("sender:" <+> pretty u)
@@ -35,15 +37,15 @@ prettyFlag False x = mempty
 prettyInput IRCInput{..} =
       prettyTime inputTime
   <+> angles (prettyTarget inputFrom)
-  <> prettySender inputSender
-  <> prettyFlag inputHasCmdPrefix "cmdPrefix"
-  <> prettyFlag inputBotAddressed "addressed"
+  <>  prettySender inputSender
+  <>  prettyFlag inputHasCmdPrefix "cmdPrefix"
+  <>  prettyFlag inputBotAddressed "addressed"
   <>  ":"
   <>  pretty inputBody
 
 prettyOutput IRCOutput{..} =
       prettyTime outputTime
   <+> angles (prettyTarget outputTo)
-  <> prettyFlag outputIsNotice "notice"
+  <>  prettyFlag outputIsNotice "notice"
   <>  ":"
   <>  pretty outputBody

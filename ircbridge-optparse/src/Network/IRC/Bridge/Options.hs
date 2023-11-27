8f74dc1ba9345eaa77a61b6110d940f1799eb111
Richard Marko <srk@48.io>
2023-11-27 17:19:51 +0100

    optparse: add help texts to cmdline options

--- ircbridge-optparse/src/Network/IRC/Bridge/Options.hs
+++ ircbridge-optparse/src/Network/IRC/Bridge/Options.hs
@@ -18,8 +18,16 @@ import Network.IRC.Bridge.Types
 
 parseTarget :: Parser IRCTarget
 parseTarget =
-      (IRCUser <$> strOption (long "user" <> short 'u'))
-  <|> (IRCChannel <$> strOption (long "chan" <> short 'c'))
+      (IRCUser <$> strOption
+        (  long "user"
+        <> short 'u'
+        <> help "Send to user (cat) or filter only messages from user (tail)"
+        ))
+  <|> (IRCChannel <$> strOption
+        (  long "chan"
+        <> short 'c'
+        <> help "Send to channel (cat) or filter only messages from channel (tail)"
+        ))
 
 data CatOpts = CatOpts {
     catTarget :: IRCTarget
@@ -30,24 +38,50 @@ data CatOpts = CatOpts {
 parseCatOptions :: Parser CatOpts
 parseCatOptions = CatOpts
   <$> parseTarget
-  <*> strArgument (metavar "BODY")
-  <*> switch (long "notice" <> short 'n')
+  <*> strArgument
+        (  metavar "BODY"
+        <> help "Message body"
+        )
+  <*> switch
+        (  long "notice"
+        <> short 'n'
+        <> help "Send message as /notice"
+        )
 
 data TailOpts = TailOpts {
     tailTarget     :: Maybe IRCTarget
   , tailOutputMode :: Maybe OutputMode
-  , tailOneShot    :: Bool -- ^ Exit after outputting a message
+  , tailOneShot    :: Bool -- ^ Exit after receiving and printing one message
   } deriving (Eq, Show, Ord)
 
 parseTailOptions :: Parser TailOpts
 parseTailOptions = TailOpts
   <$> optional parseTarget
   <*> optional parseOutputMode
-  <*> switch (long "oneshot" <> short '1')
+  <*> switch
+        (  long "oneshot"
+        <> short '1'
+        <> help "Exit after receiving and printing one message"
+        )
 
 parseOutputMode :: Parser OutputMode
 parseOutputMode =
-      flag' ShowOnly (long "show" <> short 'w')
-  <|> flag' PrettySimple (long "simple" <> short 's')
-  <|> flag' Pretty (long "pretty" <> short 'p')
-  <|> flag' PrettyDull (long "pretty-dull" <> short 'd')
+      flag' ShowOnly
+          (  long "show"
+          <> help "Print messages using Haskell show, very boring"
+          )
+  <|> flag' PrettySimple
+          (  long "simple"
+          <> short 's'
+          <> help "Print messages using pretty-simple, similar to show but with colors"
+          )
+  <|> flag' Pretty
+          (  long "pretty"
+          <> short 'p'
+          <> help "Pretty colored output"
+          )
+  <|> flag' PrettyDull
+          (  long "pretty-dull"
+          <> short 'd'
+          <> help "Like pretty but a bit dull (no colors)"
+          )

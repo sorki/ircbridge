362b2989f0ea8d57c78f343173e4386ab61be141
Richard Marko <srk@48.io>
2023-11-28 10:09:09 +0100

    finish ircbridge-amqp-irccat

--- ircbridge-amqp-irccat/Cat.hs
+++ ircbridge-amqp-irccat/Cat.hs
@@ -1,21 +1,46 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
 import Options.Applicative
+import qualified Data.Text
+import qualified Data.Text.IO
+import qualified Data.Time.Clock
+import qualified Data.Maybe
 
 import Network.IRC.Bridge.Options
-import Network.IRC.Bridge.Types
+import Network.IRC.Bridge.Types (IRCTarget, forChannel)
+import Network.IRC.Bridge.IRCCat
 import Network.IRC.Bridge.AMQP
 
+defTarget :: IRCTarget
+defTarget = either undefined id $ forChannel "#bottest"
+
 main :: IO ()
-main = execParser opts >>= \CatOpts{..} -> do
-  msg <- mkIRCOutput catTarget catBody catNotice
-  publishIRCOutput msg
+main = do
+  IRCCatOpts{..} <- execParser opts
+
+  now <- Data.Time.Clock.getCurrentTime
+
+  let failIfDefaultNeeded =
+        Data.Maybe.fromMaybe
+          (error "No target in input message and no --chan or --user option specified")
+
+  parseLikeIRCCat
+    (failIfDefaultNeeded irccatTarget)
+    now
+    irccatNotice
+      <$> Data.Text.IO.getContents
+      >>= \case
+        Left e -> error $ Data.Text.unpack e
+        Right msgs -> do
+          print msgs
+          publishIRCOutputs msgs
   where
     opts =
       info
-        (parseCatOptions <**> helper)
+        (parseIRCCatOptions <**> helper)
         (  fullDesc
         <> header "ircbridge-amqp-irccat - send a message to IRC over AMQP, piped into stdin a la irccat"
         )
